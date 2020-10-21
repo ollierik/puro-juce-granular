0f9a8f5042d902a0cc91161699be61df616db0fc
Olli Erik Keskinen <olli@oeksound.com>
2020-10-21 12:38:32 +0300

    Added comments to code

--- Source/PuroEngine.h
+++ Source/PuroEngine.h
@@ -12,6 +12,7 @@
 
 #include "puro.hpp"
 
+/** A grain is a single audio playback instance. */
 struct Grain
 {
     Grain(int offset, int length, int startIndex, float panning, double velocity, int sourceLength)
@@ -36,6 +37,9 @@ struct Grain
     puro::PanCoeffs<float, 2> panCoeffs;
 };
 
+/** Context is passed to process_grain function, and may contain persistent data or memory for the algorithm.
+    In this case, it holds temp buffers to do the audio processing on.
+ */
 struct Context
 {
     puro::heap_block_pool<float, std::allocator<float>> context_pool;
@@ -45,23 +49,33 @@ struct Context
     Context() : temp(MAX_NUM_CHANNELS, MAX_BLOCK_SIZE, context_pool), envelope(MAX_BLOCK_SIZE, context_pool) {}
 };
 
+/** The actual beef of the processor, this function gets called for every grain in every block
+    It receives a buffer dst that the grain should add its signal into.
+ */
 template <typename BufferType, typename ElementType, typename ContextType, typename SourceType>
 bool process_grain(BufferType dst, ElementType& grain, ContextType& context, const SourceType source)
 {
+    // Crop the dst buffer to fit the grain's offset and length, i.e. if the grain starts in the middle of the block, or ends
+    // before the end of the block. Update the dst buffer and grain.alignment.
     std::tie(dst, grain.alignment) = puro::alignment_advance_and_crop_buffer(dst, grain.alignment);
-    //auto audio = context.temp.template as_buffer<SourceType> (); // clang "feature", requires the keyword "template" with the function call
-    auto audio = context.temp.template as_buffer<SourceType>().trunc(dst.length());
-
+    
+    // audio buffer uses the memory from context.temp, casting it into a buffer with compile-time constant number of channels, and truncating it to needed length
+    auto audio = context.temp.template as_buffer<SourceType>().trunc(dst.length()); // clang "feature", requires the keyword "template" with the function call
+    
+    // read audio from source, cubic interpolation
     grain.readPos = puro::interp3_fill(audio, source, grain.readPos, grain.readInc);
     
-    auto envelope = context.envelope.sub(0, audio.length());
+    // truncate temp envelope buffer to fit the length of our audio, and fill
+    auto envelope = context.envelope.trunc(audio.length());
     grain.envelopePos = puro::envelope_halfcos_fill(envelope, grain.envelopePos, grain.envelopeInc);
 
     puro::multiply(audio, envelope);
 
-    BufferType output = dst.sub(0, envelope.length());
+    // get the cropped output segment that we want to add our output to, apply panning and add the audio signal
+    BufferType output = dst.trunc(envelope.length());
     puro::pan_apply_and_add(output, audio, grain.panCoeffs);
 
+    // return true if should be removed
     return (grain.alignment.remaining <= 0) // grain has depleted
             || (output.length() != dst.length()); // we've run out of source material to read
 }
@@ -107,6 +121,10 @@ public:
         {
             errorif(true, "source buffer channel number not supported");
         }
+        else
+        {
+            // if numChannels == 0, the audio source file has not been loaded, and we do nothing
+        }
     }
 
     template <typename BufferType, typename SourceType>
@@ -114,6 +132,7 @@ public:
     {
         const int blockSize = buffer.length();
 
+        // iterate all grains, adding their output to audio buffer
         for (auto&& it : pool)
         {
             if (process_grain(buffer, it.get(), context, source))
@@ -122,26 +141,33 @@ public:
             }
         }
 
+        // add new grains
         int n = buffer.length();
         while ((n = timer.advance(n)) > 0)
         {
+            // for each tick, reset interval, get new values for the grain, and create grain
+            
             const float interval = intervalParam.get();
             timer.interval = puro::math::round(durationParam.centre / interval);
             
-            if (timer.interval < 0)
-                errorif(true, "Well this is unexpected");
+            errorif(timer.interval < 0, "Well this is unexpected, timer shouldn't be let to do that");
             
             const int duration = durationParam.get();
             const float panning = panningParam.get();
             const int readpos = readposParam.get();
             const float velocity = velocityParam.get();
 
+            // push to the pool of grains, get a handle in return
             auto it = pool.push(Grain(blockSize - n, duration, readpos, panning, velocity, sourceBuffer.length()));
 
+            // immediately process the first block for the newly created grain, potentially also already removing it
+            // if the pool is full, it rejects the push operation and returns invalid iterator, and the grain doesn't get added
             if (it.is_valid())
             {
                 if (process_grain(buffer, it.get(), context, source))
+                {
                     pool.pop(it);
+                }
             }
         }
     }
